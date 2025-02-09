2e1be7cde9298a38225a43e46433c684020aef2e
Petar <iosdev.petar@gmail.com>
2025-02-09 11:29:48 +0100

    Initial commit. Configured project, tests, fastlane, GitHubActions

---
+++ Quizzy/QuizzyApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuizzyApp.swift
+//  Quizzy
+//
+//  Created by Petar  on 9.2.25..
+//
+
+import SwiftUI
+
+@main
+struct QuizzyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ QuizzyTests/QuizzyTests.swift
@@ -0,0 +1,12 @@
+//
+//  QuizzyTests.swift
+//  QuizzyTests
+//
+//  Created by Petar  on 9.2.25..
+//
+
+import XCTest
+
+final class QuizzyTests: XCTestCase {
+
+}
